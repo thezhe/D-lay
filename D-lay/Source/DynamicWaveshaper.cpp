3e42d47e9a67e6e64b01bb8bc92f9d63d8c18a11
thezhe <tigr2.718@gmail.com>
2019-12-31 22:19:50 -0500

    refactored DynamicWaveshaper and added SIMD optimization

--- D-lay/Source/DynamicWaveshaper.cpp
+++ D-lay/Source/DynamicWaveshaper.cpp
@@ -14,37 +14,47 @@ void DynamicWaveshaper::prepare(dsp::ProcessSpec spec)
 	mNumChannels = spec.numChannels;
 	mSampleRate = spec.sampleRate;
 
-	//keep track of max value in chunks sampled at 200Hz
+	//keep track of max value in chunks sampled at 100Hz
 	mChunkSize = spec.sampleRate / 100.0f;
 
-	//envelope processing arrays
+#if JUCE_USE_SIMD
+	//prepare for channel interleaving
+	mInterleaved = dsp::AudioBlock<dsp::SIMDRegister<float>>(interleavedBlockData, 1, mBlockSize);
+	mZero = dsp::AudioBlock<float>(zeroData, dsp::SIMDRegister<float>::size(), mBlockSize);
+	mZero.clear();
+#else
 	mChunkMaxIn = std::make_unique<float[]>(mNumChannels);
-	mChunkCounter = std::make_unique<int[]>(mNumChannels);
 	mSideChainThreshIn = std::make_unique<float[]>(mNumChannels);
-	mLastSample = std::make_unique<float[]>(mNumChannels); //set up zeroed y_n-1 elements for each channel to prepare for IIR envelope smoothing
+	mLastSample = std::make_unique<float[]>(mNumChannels); //set up zeroed y[n-1] array elements each channel to prepare for IIR envelope smoothing
+#endif	
 
-	//initialize waveshapers and sidechain input
-	mTargetWaveshaper.initialise([](float x) {return tanh(20*x); }, -1.0f, 1.0f, 512);
+	//initialize waveshapers and side chain signal
+	mTargetWaveshapers.ensureStorageAllocated(1);
+	//mTargetWaveshapers.add (std::make_unique<dsp::LookupTableTransform<float>>([](float x) {return tanh(20 * x); }, -1.0f, 1.0f, 512));
+	mTargetWaveshapers.add(std::make_unique<dsp::LookupTableTransform<float>>([](float x) {return tanh(20 * x); }, -1.0f, 1.0f, 512));
+	mTargetWaveshapers.minimiseStorageOverheads();
+	mSideChain.setSize(mNumChannels, mBlockSize);
 
 	//initialize parameters
 	setAttack(50.0f);
 	setRelease(100.0f);
 }
 
-void DynamicWaveshaper::setThreshold(float dbThreshold) noexcept 
+void DynamicWaveshaper::setThreshold(float dbThreshold) noexcept
 {
 	jassert(dbThreshold <= 0.0f);
 	mThreshold = Decibels::decibelsToGain(dbThreshold);
 }
 
-void DynamicWaveshaper::setAttack(float msAttack) noexcept 
+void DynamicWaveshaper::setAttack(float msAttack) noexcept
 {
 	jassert(msAttack >= 0);
 	mAttackCoeff = exp(-1000 / (msAttack * mSampleRate));
 }
 
-void DynamicWaveshaper::setRelease(float msRelease) noexcept 
+void DynamicWaveshaper::setRelease(float msRelease) noexcept
 {
 	jassert(msRelease >= 0);
 	mReleaseCoeff = exp(-1000 / (msRelease * mSampleRate));
+
 }
--- D-lay/Source/DynamicWaveshaper.h
+++ D-lay/Source/DynamicWaveshaper.h
@@ -9,7 +9,6 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-//A "dynamic" waveshaper that interpolates between dry signal and waveshaped signal based on input signal envelope. Loosely follows JUCE's dsp processor format.
 class DynamicWaveshaper
 {
 public:
@@ -17,7 +16,7 @@ public:
 	// Essential Methods
 	//==============================================================================
 
-	//save environment variables and waveshaper/sidechain
+	//save environment variables and waveshaper/en
 	void prepare(dsp::ProcessSpec spec);
 
 	//extract thresholded, smoothed signal envelope and apply weighted waveshaper of choice
@@ -36,38 +35,15 @@ public:
 		else
 		{
 			updateBufParams();
-			//process sidechain and apply amount of waveshaping proportional to it
-
-			
-
-			for (int channel = 0; channel < mNumChannels; ++channel)
-			{
-				for (int i = 0; i < mBlockSize; ++i)
-				{
-					//use step response of a one pole low pass filter (IIR) to apply attack and release parameters to binary threshold input
-					if (mSideChainThreshIn[channel] > mLastSample[channel]) {
-						mLastSample[channel] = mBufAttackCoeff * mLastSample[channel] + (1 - mBufAttackCoeff) * mSideChainThreshIn[channel]; // y_n = a*y_n-1 + (1-a)*x_n
-					}
-					else {
-						mLastSample[channel] = mBufReleaseCoeff * mLastSample[channel]; //y_n = r*y_n-1
-					}
-					//apply waveshaping with smoothed envelope the weight
+			updateSideChain(inputBlock);
+			//apply amount of waveshaping proportional to sidechain signal
+			for (int channel = 0; channel < mNumChannels; ++channel) {
+				for (int i = 0; i < mBlockSize; ++i) {
 					outputBlock.getChannelPointer(channel)[i] = std::lerp(
 						inputBlock.getChannelPointer(channel)[i],
-						mTargetWaveshaper.processSampleUnchecked(inputBlock.getChannelPointer(channel)[i]),
-						mLastSample[channel]
+						mTargetWaveshapers.getUnchecked(0)->processSampleUnchecked(inputBlock.getChannelPointer(channel)[i]),
+						mSideChain.getSample(channel, i)
 					);
-					//update max value in chunk
-					if (abs(inputBlock.getChannelPointer(channel)[i]) > mChunkMaxIn[channel])
-						mChunkMaxIn[channel] = abs(inputBlock.getChannelPointer(channel)[i]);
-					//save threshold of max value and reset for next chunk
-					if (++mChunkCounter[channel] == mChunkSize)
-					{
-						if (mChunkMaxIn[channel] > mBufThreshold) { mSideChainThreshIn[channel] = 1.0f; }
-						else { mSideChainThreshIn[channel] = 0.0f; }
-						mChunkCounter[channel] = 0;
-						mChunkMaxIn[channel] = 0.0f;
-					}
 				}
 			}
 		}
@@ -87,13 +63,126 @@ public:
 
 private:
 
-	//target waveshapers
-	dsp::LookupTableTransform<float> mTargetWaveshaper;
+	//save an audio block's signal envelope to a side chain buffer using Threshold, Attack, and Release parameters(call once per process after updateBufParams)
+	void updateSideChain(const dsp::AudioBlock<const float>& inputBlock) noexcept
+	{
+#if JUCE_USE_SIMD
+		//=======================interleave inputBlock for processing
+		auto* inout = mChannelPointers.getData();
+		for (size_t channel = 0; channel < dsp::SIMDRegister<float>::size(); ++channel)
+			inout[channel] = (channel < inputBlock.getNumChannels()) ? const_cast<float*> (inputBlock.getChannelPointer(channel)) : mZero.getChannelPointer(channel); //fill extra channels with zero data
+		AudioDataConverters::interleaveSamples(inout, reinterpret_cast<float*> (mInterleaved.getChannelPointer(0)), //make sure to set float granularity
+			mBlockSize, static_cast<int> (dsp::SIMDRegister<float>::size()));
+		//=======================process mInterleaved data
+		//process first samples
+		mChunkMaxIn = dsp::SIMDRegister<float>::max(dsp::SIMDRegister<float>::abs(mInterleaved.getChannelPointer(0)[0]), mChunkMaxIn); //get max before processing
+		auto iirMask = dsp::SIMDRegister<float>::greaterThan(mSideChainThreshIn, mLastSample);
+		mInterleaved.getChannelPointer(0)[0] = ((mBufAttackCoeff * mLastSample + ((ONE - mBufAttackCoeff) * mSideChainThreshIn)) & iirMask) 
+			+ ((mBufReleaseCoeff*mLastSample) & (~iirMask));
+		if (++mChunkCounter == mChunkSize)
+		{
+			mChunkCounter = 0;
+			auto maxMask = dsp::SIMDRegister<float>::greaterThan(mChunkMaxIn, mBufThreshold);
+			mSideChainThreshIn = (ONE & maxMask) + (ZERO & (~maxMask));
+			mChunkMaxIn = 0.0f;
+		}
+		for (int i = 1; i < mBlockSize; ++i) {
+			mChunkMaxIn = dsp::SIMDRegister<float>::max(dsp::SIMDRegister<float>::abs(mInterleaved.getChannelPointer(0)[i]), mChunkMaxIn);
+			auto iirLoopMask = dsp::SIMDRegister<float>::greaterThan(mSideChainThreshIn, mInterleaved.getChannelPointer(0)[i-1]);
+			mInterleaved.getChannelPointer(0)[i] = ((mBufAttackCoeff * mInterleaved.getChannelPointer(0)[i - 1] + ((ONE - mBufAttackCoeff) * mSideChainThreshIn)) & iirLoopMask) 
+				+ ((mBufReleaseCoeff * mInterleaved.getChannelPointer(0)[i - 1]) & (~iirLoopMask));
+			if (++mChunkCounter == mChunkSize)
+			{
+				mChunkCounter = 0;
+				auto maxMask = dsp::SIMDRegister<float>::greaterThan(mChunkMaxIn, mBufThreshold);
+				mSideChainThreshIn = (ONE & maxMask) + (ZERO & (~maxMask));
+				mChunkMaxIn = 0.0f;
+			}
+		}
+		mLastSample = mInterleaved.getChannelPointer(0)[mBlockSize - 1];
+		//=======================deinterleave
+		for (size_t channel = 0; channel < inputBlock.getNumChannels(); ++channel)
+			inout[channel] = mSideChain.getWritePointer(channel);
+		AudioDataConverters::deinterleaveSamples(reinterpret_cast<float*> (mInterleaved.getChannelPointer(0)),
+				const_cast<float**> (inout), mBlockSize, static_cast<int> (dsp::SIMDRegister<float>::size()));
+#else
+		//process first samples
+		for (int channel = 0; channel < mNumChannels; ++channel)
+		{
+			//use step response of a one pole low pass filter (IIR) to apply attack and release parameters to binary side chain
+			if (mSideChainThreshIn[channel] > mLastSample[channel]) {	
+				mSideChain.getWritePointer(channel)[0] = mBufAttackCoeff * mLastSample[channel] + ((1 - mBufAttackCoeff) * mSideChainThreshIn[channel]);
+			}
+			else {
+				mSideChain.getWritePointer(channel)[0] = mBufReleaseCoeff * mLastSample[channel];
+			}
+			//update max value in chunk
+			if (abs(inputBlock.getChannelPointer(channel)[0]) > mChunkMaxIn[channel])
+				mChunkMaxIn[channel] = abs(inputBlock.getChannelPointer(channel)[0]);
+		}
+		//threshold max value and reset for next chunk
+		if (++mChunkCounter == mChunkSize)
+		{
+			mChunkCounter = 0;
+			for (int channel = 0; channel < mNumChannels; ++channel)
+			{
+				if (mChunkMaxIn[channel] > mBufThreshold) { mSideChainThreshIn[channel] = 1.0f; }
+				else { mSideChainThreshIn[channel] = 0.0f; }
+				mChunkMaxIn[channel] = 0.0f;
+			}
+		}
+		//process rest of samples
+		for (int i = 1; i < mBlockSize; ++i)
+		{
+			for (int channel = 0; channel < mNumChannels; ++channel)
+			{
+				if (mSideChainThreshIn[channel] > mSideChain.getWritePointer(channel)[i-1]) {
+					//use step response of a one pole low pass filter (IIR) to apply attack and release parameters to binary side chain
+					mSideChain.getWritePointer(channel)[i] = mBufAttackCoeff * mSideChain.getWritePointer(channel)[i - 1] + ((1 - mBufAttackCoeff) * mSideChainThreshIn[channel]);
+				}
+				else {
+					mSideChain.getWritePointer(channel)[i] = mBufReleaseCoeff * mSideChain.getWritePointer(channel)[i - 1];
+				}
+				//update max value in chunk
+				if (abs(inputBlock.getChannelPointer(channel)[i]) > mChunkMaxIn[channel])
+					mChunkMaxIn[channel] = abs(inputBlock.getChannelPointer(channel)[i]);
+			}
+			//threshold max value and reset for next chunk
+			if (++mChunkCounter == mChunkSize)
+			{
+				mChunkCounter = 0;
+				for (int channel = 0; channel < mNumChannels; ++channel)
+				{
+					if (mChunkMaxIn[channel] > mBufThreshold) { mSideChainThreshIn[channel] = 1.0f; }
+					else { mSideChainThreshIn[channel] = 0.0f; }
+					mChunkMaxIn[channel] = 0.0f;
+				}
+			}
+		}
+		//save last sample
+		for (int channel = 0; channel < mNumChannels; ++channel)
+		{
+			mLastSample[channel] = mSideChain.getWritePointer(channel)[mBlockSize-1];
+		}
+#endif
+	}
+
+	//dynamic waveshaping variables
+	OwnedArray<dsp::LookupTableTransform<float>> mTargetWaveshapers; //smart array that deletes objects in destructor
+	AudioBuffer<float> mSideChain;
 
 	//envelope variables
-	std::unique_ptr<float[]> mLastSample, mChunkMaxIn, mSideChainThreshIn; //use smart pointer to construct array of zeros in prepare and auto delete in destructor
-	std::unique_ptr<int[]> mChunkCounter;
-	int mChunkSize;
+	int mChunkSize, mChunkCounter = 0;
+#if JUCE_USE_SIMD
+	dsp::SIMDRegister<float> mLastSample = 0, mChunkMaxIn = 0, mSideChainThreshIn = 0;
+	//data used to interleave mChannelPointers data into interleavedBlockData
+	dsp::AudioBlock<dsp::SIMDRegister<float>> mInterleaved;
+	dsp::AudioBlock<float> mZero;
+	HeapBlock<char> interleavedBlockData, zeroData;
+	HeapBlock<const float*> mChannelPointers{ dsp::SIMDRegister<float>::size() };
+#else
+	std::unique_ptr<float[]> mLastSample, mChunkMaxIn, mSideChainThreshIn; //use smart pointer to construct in prepare and auto delete in destructor
+#endif
 
 	//called once per process
 	void updateBufParams() noexcept
@@ -104,12 +193,15 @@ private:
 	}
 
 	//parameters updated via Atomic loads once per buffer
+#if JUCE_USE_SIMD
+	dsp::SIMDRegister<float> mBufThreshold, mBufAttackCoeff, mBufReleaseCoeff;
+	const dsp::SIMDRegister<float> ONE = 1.0f, ZERO = 0.0f;
+#else
 	float mBufThreshold, mBufAttackCoeff, mBufReleaseCoeff;
-
+#endif
 	//instantaneous processing parameters wrapped in Atomic for thread safety (units: gain, coefficient, coefficient)
-	Atomic<float> mThreshold = 0.1f, mAttackCoeff, mReleaseCoeff;
+	Atomic<float> mThreshold = 0.1f, mAttackCoeff = 0.99f, mReleaseCoeff = 0.99f;
 
 	//environment variables
 	int mBlockSize, mNumChannels, mSampleRate;
 };
-//TODO SIMD
